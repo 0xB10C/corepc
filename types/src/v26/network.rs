9e4eb4d7181575ff75f9e0a6997b091bd5023b1d
0xb10c <b10c@b10c.me>
2025-11-24 17:49:43 +0100

    patch: add inv_to_send and cpu_info for getpeerinfo

--- types/src/v26/network.rs
+++ types/src/v26/network.rs
@@ -154,4 +154,15 @@ pub struct PeerInfo {
     pub transport_protocol_type: String,
     /// The session ID for this connection, or "" if there is none ("v2" transport protocol only).
     pub session_id: String,
+
+    // Patched for peer-observer rpc-extractor:
+
+    /// From https://github.com/bitcoin/bitcoin/pull/33448:
+    /// How many txs we have queued to announce to this peer
+    pub inv_to_send: Option<usize>,
+    /// From https://github.com/bitcoin/bitcoin/pull/31672:
+    /// Total CPU time spent processing messages to/from the peer, in per milles (‰) of the connection duration,
+    /// if supported by the platform and measured. High CPU time is not necessarily a bad thing - new valid
+    /// transactions and blocks require it be validated.
+    pub cpu_load: Option<f32>,
 }
